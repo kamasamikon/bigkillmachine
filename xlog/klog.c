3d24a704be83eaeb0db84acd50b3f95bea383eab
kamasamikon <kamasamikon@gmail.com>
2013-09-05 14:54:15 +0800

    split xlog.c to klog.c and klog.h

--- xlog/klog.c
+++ xlog/klog.c
@@ -13,91 +13,87 @@
 #include <kflg.h>
 #include <xtcool.h>
 
+#include "xlog.h"
+
 /*-----------------------------------------------------------------------
  * xlog.h part
  */
 
+/* STRing ARRay to save programe name, module name, file name etc */
+typedef struct _strarr_t strarr_t;
+struct _strarr_t {
+	int size;
+	int cnt;
+	char **arr;
+};
+
+typedef struct _rule_t rule_t;
+struct _rule_t {
+	/* XXX: Don't filter ThreadID, it need the getflg called every log */
+
+	/* 0 is know care */
+	/* -1 is all */
+
+	int prog;	/* Program command line */
+	int modu;	/* Module name */
+	int file;	/* File name */
+	int func;	/* Function name */
+
+	int line;	/* Line number */
+
+	int pid;	/* Process ID */
+
+	/* Which flag to be set or clear */
+	unsigned int set, clr;
+};
+
+typedef struct _rulearr_t rulearr_t;
+struct _rulearr_t {
+	int size;
+	int cnt;
+	rule_t *arr;
+};
+
+typedef struct _flgmap_t flgmap_t;
+struct _flgmap_t {
+	char code;
+	unsigned int bit;
+};
+
+static flgmap_t __g_flgmap[] = {
+	{ 't', KLOG_TRC },
+	{ 'l', KLOG_LOG },
+	{ 'e', KLOG_ERR },
+	{ 'f', KLOG_FAT },
+
+	{ 's', KLOG_RTM },
+	{ 'S', KLOG_ATM },
+
+	{ 'j', KLOG_PID },
+	{ 'x', KLOG_TID },
+
+	{ 'N', KLOG_LINE },
+	{ 'F', KLOG_FILE },
+	{ 'M', KLOG_MODU },
+	{ 'H', KLOG_FUNC },
+	{ 'P', KLOG_PROG },
+};
+
+/*-----------------------------------------------------------------------
+ * xlog.c part
+ */
+static strarr_t sa_file_name = { 0 };
+static strarr_t sa_modu_name = { 0 };
+static strarr_t sa_prog_name = { 0 };
+static strarr_t sa_func_name = { 0 };
+
+static rulearr_t rulearr = { 0 };
+
 inline int klog_ver();
 void parse_flg(const char *flg, unsigned int *set, unsigned int *clr);
 
-#define KLOG_TRC         0x00000001 /* t: Trace */
-#define KLOG_LOG         0x00000002 /* l: Log */
-#define KLOG_ERR         0x00000004 /* e: Error */
-#define KLOG_FAT         0x00000008 /* f: Fatal Error */
-#define KLOG_TYPE_ALL    0x0000000f
-
-#define KLOG_RTM         0x00000100 /* s: Relative Time, in MS, 'ShiJian' */
-#define KLOG_ATM         0x00000200 /* S: ABS Time, in MS, 'ShiJian' */
-
-#define KLOG_PID         0x00001000 /* j: Process ID, 'JinCheng' */
-#define KLOG_TID         0x00002000 /* x: Thread ID, 'XianCheng' */
-
-#define KLOG_PROG        0x00010000 /* P: Process Name */
-#define KLOG_MODU        0x00020000 /* M: Module Name */
-#define KLOG_FILE        0x00040000 /* F: File Name */
-#define KLOG_FUNC        0x00080000 /* H: Function Name, 'HanShu' */
-#define KLOG_LINE        0x00100000 /* N: Line Number */
-
-/* __FILE__ 的名字都比较奇怪，应该去掉路径部分，只保留文件名 */
-static int __file_name_id = -1;
-static char *__file_name = NULL;
-
-/* 进程的名字可能不总是需要，所以没有必要总是算这个 */
-static int __prog_name_id = -1;
-static char *__prog_name = NULL;
-
-/* 模块的名字是需要的 */
-static int __modu_name_id = -1;
-
-#define O_LOG_COMPNAME "MODU_XLOG"
-
-#if (defined(O_LOG_COMPNAME))
-#define COMP_NAME  O_LOG_COMPNAME
-#elif (defined(PACKAGE_NAME))
-#define COMP_NAME  PACKAGE_NAME
-#else
-#define COMP_NAME  "?"
-#endif
-
-#define MODU_NAME COMP_NAME
-
-#define INNER_VAR_DEF() \
-	static int ver_sav = -1; \
-static int func_name_id = -1; \
-static int flg = 0; \
-int ver_get = klog_ver()
-
-#define SETUP_NAME_AND_ID() do { \
-	if (__file_name_id == -1) { \
-		__file_name = only_name(__FILE__); \
-		__file_name_id = file_name_add(__file_name); \
-	} \
-	if (__prog_name_id == -1) { \
-		__prog_name = get_prog_name(); \
-		__prog_name_id = prog_name_add(__prog_name); \
-	} \
-	if (__modu_name_id == -1) { \
-		__modu_name_id = modu_name_add(MODU_NAME); \
-	} \
-	if (func_name_id == -1) { \
-		func_name_id = func_name_add(__func__); \
-	} \
-} while (0)
-
 int rlogf(unsigned char type, unsigned int flg, const char *prog, const char *modu, const char *file, const char *func, int ln, const char *fmt, ...)
 {
-#if 0
-	printf("type: %c\n", type);
-	printf("flag: %x\n", flg);
-	printf("prog: %s\n", prog);
-	printf("modu: %s\n", modu);
-	printf("file: %s\n", file);
-	printf("func: %s\n", func);
-	printf("line: %d\n", ln);
-	printf("fmt: %s\n", fmt);
-	return 0;
-#endif
-
 	// klogcc_t *cc = (klogcc_t*)klog_cc();
 	va_list ap;
 	char buffer[2048], *bufptr = buffer;
@@ -222,19 +218,6 @@ int rlogf(unsigned char type, unsigned int flg, const char *prog, const char *mo
 
 }
 
-#define klog(fmt, ...) do { \
-	INNER_VAR_DEF(); \
-	if (ver_get > ver_sav) { \
-		ver_sav = ver_get; \
-		SETUP_NAME_AND_ID(); \
-		flg = klog_calc_flg(__prog_name_id, __modu_name_id, __file_name_id, func_name_id, __LINE__, getpid()); \
-		if (!(flg & KLOG_LOG)) \
-		flg = 0; \
-	} \
-	if (flg) \
-	rlogf('L', flg, __prog_name, MODU_NAME, __file_name, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__); \
-} while (0)
-
 char *only_name(char *name)
 {
 	char *dup = strdup(name);
@@ -269,47 +252,6 @@ inline int klog_ver()
 {
 	return 2;
 }
-
-struct _name_arr_t {
-	char *name;
-	int nr;
-};
-
-typedef struct _rule_t rule_t;
-struct _rule_t {
-	/* XXX: Don't filter ThreadID, it need the getflg called every log */
-
-	/* 0 is know care */
-	/* -1 is all */
-
-	int prog;	/* Program command line */
-	int modu;	/* Module name */
-	int file;	/* File name */
-	int func;	/* Function name */
-
-	int line;	/* Line number */
-
-	int pid;	/* Process ID */
-
-	/* After parse =leftXXX */
-	unsigned int set, clr;
-};
-
-
-typedef struct _strarr_t strarr_t;
-struct _strarr_t {
-	int size;
-	int cnt;
-	char **arr;
-};
-
-/*-----------------------------------------------------------------------
- * xlog.c part
- */
-static strarr_t sa_file_name = { 0 };
-static strarr_t sa_modu_name = { 0 };
-static strarr_t sa_prog_name = { 0 };
-static strarr_t sa_func_name = { 0 };
 
 static int strarr_find(strarr_t *sa, const char *str)
 {
@@ -372,14 +314,6 @@ int func_name_find(const char *name)
 }
 
 
-typedef struct _rulearr_t rulearr_t;
-struct _rulearr_t {
-	int size;
-	int cnt;
-	rule_t *arr;
-};
-
-static rulearr_t rulearr = { 0 };
 
 static int rulearr_add(rulearr_t *ra, int prog, int modu,
 		int file, int func, int line, int pid,
@@ -448,38 +382,14 @@ void rule_add(const char *rule)
 }
 
 
-typedef struct _flgmap_t flgmap_t;
-struct _flgmap_t {
-	char code;
-	unsigned int bit;
-};
-
-static flgmap_t flgmap[] = {
-	{ 't', KLOG_TRC },
-	{ 'l', KLOG_LOG },
-	{ 'e', KLOG_ERR },
-	{ 'f', KLOG_FAT },
-
-	{ 's', KLOG_RTM },
-	{ 'S', KLOG_ATM },
-
-	{ 'j', KLOG_PID },
-	{ 'x', KLOG_TID },
-
-	{ 'N', KLOG_LINE },
-	{ 'F', KLOG_FILE },
-	{ 'M', KLOG_MODU },
-	{ 'H', KLOG_FUNC },
-	{ 'P', KLOG_PROG },
-};
 
 unsigned int get_flg(char c)
 {
 	int i;
 
-	for (i = 0; i < sizeof(flgmap) / sizeof(flgmap[0]); i++)
-		if (flgmap[i].code == c)
-			return flgmap[i].bit;
+	for (i = 0; i < sizeof(__g_flgmap) / sizeof(__g_flgmap[0]); i++)
+		if (__g_flgmap[i].code == c)
+			return __g_flgmap[i].bit;
 	return 0;
 }
 
---
+++ xlog/xlog.h
@@ -0,0 +1,101 @@
+/* vim:set noet ts=8 sw=8 sts=8 ff=unix: */
+
+#ifndef __K_LOG_H__
+#define __K_LOG_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+inline int klog_ver();
+void parse_flg(const char *flg, unsigned int *set, unsigned int *clr);
+
+#define KLOG_TRC         0x00000001 /* t: Trace */
+#define KLOG_LOG         0x00000002 /* l: Log */
+#define KLOG_ERR         0x00000004 /* e: Error */
+#define KLOG_FAT         0x00000008 /* f: Fatal Error */
+#define KLOG_TYPE_ALL    0x0000000f
+
+#define KLOG_RTM         0x00000100 /* s: Relative Time, in MS, 'ShiJian' */
+#define KLOG_ATM         0x00000200 /* S: ABS Time, in MS, 'ShiJian' */
+
+#define KLOG_PID         0x00001000 /* j: Process ID, 'JinCheng' */
+#define KLOG_TID         0x00002000 /* x: Thread ID, 'XianCheng' */
+
+#define KLOG_PROG        0x00010000 /* P: Process Name */
+#define KLOG_MODU        0x00020000 /* M: Module Name */
+#define KLOG_FILE        0x00040000 /* F: File Name */
+#define KLOG_FUNC        0x00080000 /* H: Function Name, 'HanShu' */
+#define KLOG_LINE        0x00100000 /* N: Line Number */
+
+static int __file_name_id = -1;
+static char *__file_name = NULL;
+
+static int __prog_name_id = -1;
+static char *__prog_name = NULL;
+
+static int __modu_name_id = -1;
+
+#ifndef MODU_NAME
+#define MODU_NAME  "?"
+#endif
+
+#define KLOG_INNER_VAR_DEF() \
+	static int ver_sav = -1; \
+	static int func_name_id = -1; \
+	static int flg = 0; \
+	int ver_get = klog_ver()
+
+#define KLOG_SETUP_NAME_AND_ID() do { \
+	if (__file_name_id == -1) { \
+		__file_name = only_name(__FILE__); \
+		__file_name_id = file_name_add(__file_name); \
+	} \
+	if (__prog_name_id == -1) { \
+		__prog_name = get_prog_name(); \
+		__prog_name_id = prog_name_add(__prog_name); \
+	} \
+	if (__modu_name_id == -1) { \
+		__modu_name_id = modu_name_add(MODU_NAME); \
+	} \
+	if (func_name_id == -1) { \
+		func_name_id = func_name_add(__func__); \
+	} \
+} while (0)
+
+
+#define klog(fmt, ...) do { \
+	KLOG_INNER_VAR_DEF(); \
+	if (ver_get > ver_sav) { \
+		ver_sav = ver_get; \
+		KLOG_SETUP_NAME_AND_ID(); \
+		flg = klog_calc_flg(__prog_name_id, __modu_name_id, __file_name_id, func_name_id, __LINE__, getpid()); \
+		if (!(flg & KLOG_LOG)) \
+		flg = 0; \
+	} \
+	if (flg) \
+	rlogf('L', flg, __prog_name, MODU_NAME, __file_name, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__); \
+} while (0)
+
+int file_name_add(const char *name);
+int file_name_find(const char *name);
+int modu_name_add(const char *name);
+int modu_name_find(const char *name);
+int prog_name_add(const char *name);
+int prog_name_find(const char *name);
+int func_name_add(const char *name);
+int func_name_find(const char *name);
+
+void rule_add(const char *rule);
+
+unsigned int get_flg(char c);
+
+void parse_flg(const char *flg, unsigned int *set, unsigned int *clr);
+
+int klog_calc_flg(int prog, int modu, int file, int func, int line, int pid);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* __K_LOG_H__ */
+
