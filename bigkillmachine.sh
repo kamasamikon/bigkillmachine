d1b8b3b826326eb8ffea8801e759446da4b09e99
kamasamikon <kamasamikon@gmail.com>
2013-09-09 16:19:15 +0800

    bigkillmachine.sh continue

--- bigkillmachine.sh
+++ bigkillmachine.sh
@@ -9,24 +9,36 @@ DIR_PROJ=$1
 TYPE_CONFIG=$2
 TYPE_BUILD=$3
 
-
 BR_DIR=${DIR_PROJ}/../../buildroot
+
+echo -e Hit return to continue.
+read
 
 echo "Apply the rcS patch"
 RCS_PATH=${DIR_PROJ}/${TYPE_BUILD}_${TYPE_CONFIG}/target/etc/init.d/rcS
+RCS_PATH=${DIR_PROJ}/fs/skeleton/etc/init.d/rcS
 RCS_HASH=`md5sum ${RCS_PATH} | cut -d " " -f1 `
 if [ "dcbfed08fb0852617beedb68561f37fd" != ${RCS_PATH} ]; then
     sudo meld rcS ${RCS_PATH}
 else
-    echo cp rcS ${DIR_PROJ}/${TYPE_BUILD}_${TYPE_CONFIG}/target/etc/init.d/
+    cp -v rcS ${DIR_PROJ}/${TYPE_BUILD}_${TYPE_CONFIG}/target/etc/init.d/
 fi
 
 echo "Copy libnemohook.so"
-echo cp libnemohook.so ${DIR_PROJ}/${TYPE_BUILD}_${TYPE_CONFIG}/target/usr/local/lib
+cp -v libnemohook.so ${DIR_PROJ}/${TYPE_BUILD}_${TYPE_CONFIG}/target/lib
+cp -v libnemohook.so ${DIR_PROJ}/fs/skeleton/lib
+
+cp -v libhilda.so ${DIR_PROJ}/${TYPE_BUILD}_${TYPE_CONFIG}/target/lib
+cp -v libhilda.so ${DIR_PROJ}/fs/skeleton/lib
 
 echo "Apply dbus buildroot patch"
-echo cp hook/dbus/dbus_dbus_connection_dispatch_hook.patch ${BR_DIR}/package/dbus
+cp -v hook/dbus/dbus_dbus_connection_dispatch_hook.patch ${BR_DIR}/package/dbus
 
 
 echo "Process ntvlog.h"
-
+if [ -f ${DIR_PROJ}/../../nemotv/src/utils/ntvlog.h.bak ]; then
+    echo "ntvlog.h already backuped, skip"
+else
+    mv -v ${DIR_PROJ}/../../nemotv/src/utils/ntvlog.h ${DIR_PROJ}/../../nemotv/src/utils/ntvlog.h.bak
+fi
+cp -v ntvlog.h ${DIR_PROJ}/../../nemotv/src/utils/ntvlog.h
