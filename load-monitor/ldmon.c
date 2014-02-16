aedbee5b07205ffa5d68293a1d0c761d3227889e
kamasamikon <kamasamikon@gmail.com>
2014-02-16 09:59:56 +0800

    ldmon: save info to cache before output file is ready, e.g /tmp is not mounted

--- load-monitor/ldmon.c
+++ load-monitor/ldmon.c
@@ -10,17 +10,21 @@
 
 #include <sys/sysinfo.h>
 
-#define FSHIFT 16              /* nr of bits of precision */
-#define FIXED_1      (1 << FSHIFT)     /* 1.0 as fixed-point */
-#define LOAD_INT(x)  (unsigned)((x) >> FSHIFT)
-#define LOAD_FRAC(x) LOAD_INT(((x) & (FIXED_1 - 1)) * 100)
+#define FSHIFT          16                      /* nr of bits of precision */
+#define FIXED_1         (1 << FSHIFT)           /* 1.0 as fixed-point */
+#define LOAD_INT(x)     (unsigned)((x) >> FSHIFT)
+#define LOAD_FRAC(x)    LOAD_INT(((x) & (FIXED_1 - 1)) * 100)
+
+#define CACHE_DEPT      50000
+static char *__g_cache[CACHE_DEPT];
+static int __g_cache_used = 0;
 
 int main(int argc, char *argv[])
 {
 	struct sysinfo info;
 	struct tm *current_time;
 	time_t current_secs;
-	char tm_buf[1024];
+	char tm_buf[32], cache_line[64];
 	FILE *fp = NULL;
 	int loops = -1;
 
@@ -40,11 +44,8 @@ int main(int argc, char *argv[])
 
 		if (!fp) {
 			fp = fopen(output_file, "wt");
-			if (!fp) {
+			if (!fp)
 				printf("Loop:%d, Open file '%s' failed.\n", loops, output_file);
-				usleep(1000 * pause_time);
-				continue;
-			}
 		}
 
 		sysinfo(&info);
@@ -53,11 +54,33 @@ int main(int argc, char *argv[])
 		current_time = localtime(&current_secs);
 		strftime(tm_buf, sizeof(tm_buf), "%H:%M:%S", current_time);
 
-		fprintf(fp, "%4u %s %8lu %4u %u.%02u %u.%02u %u.%02u\n",
-				loops, tm_buf, info.uptime, info.procs,
-				LOAD_INT(info.loads[0]), LOAD_FRAC(info.loads[0]),
-				LOAD_INT(info.loads[1]), LOAD_FRAC(info.loads[1]),
-				LOAD_INT(info.loads[2]), LOAD_FRAC(info.loads[2]));
+		if (fp) {
+			/* Flush all cached line */
+			if (__g_cache_used) {
+				int i;
+				for (i = 0; i < __g_cache_used; i++)
+					fprintf(fp, "%s", __g_cache[i]);
+				fflush(fp);
+				__g_cache_used = 0;
+			}
+
+			fprintf(fp, "%04u %s %8lu %4u %u.%02u %u.%02u %u.%02u\n",
+					loops, tm_buf, info.uptime, info.procs,
+					LOAD_INT(info.loads[0]), LOAD_FRAC(info.loads[0]),
+					LOAD_INT(info.loads[1]), LOAD_FRAC(info.loads[1]),
+					LOAD_INT(info.loads[2]), LOAD_FRAC(info.loads[2]));
+			fflush(fp);
+		} else {
+			/* The output file is not ready, save the information to cache */
+			snprintf(cache_line, sizeof(cache_line), "%04u %s %8lu %4u %u.%02u %u.%02u %u.%02u\n",
+					loops, tm_buf, info.uptime, info.procs,
+					LOAD_INT(info.loads[0]), LOAD_FRAC(info.loads[0]),
+					LOAD_INT(info.loads[1]), LOAD_FRAC(info.loads[1]),
+					LOAD_INT(info.loads[2]), LOAD_FRAC(info.loads[2]));
+
+			__g_cache[__g_cache_used % CACHE_DEPT] = strdup(cache_line);
+			__g_cache_used++;
+		}
 
 #if 0
 		printf("totalram:  %lu\n", info.totalram);
