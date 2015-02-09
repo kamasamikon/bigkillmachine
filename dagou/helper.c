ae9bddf0c008530d503537710afd693e22cda799
kamasamikon <kamasamikon@gmail.com>
2015-02-09 11:30:14 +0800

    add helper (it's null currently) to project

--- dagou/helper.c
+++ dagou/helper.c
@@ -1,14 +1,12 @@
 /* vim:set noet ts=8 sw=8 sts=8 ff=unix: */
 
-#include "nh_helper.h"
+#include <helper.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
 #include <time.h>
-
-#include <hilda/klog.h>
 
 /*-----------------------------------------------------------------------
  * helper
