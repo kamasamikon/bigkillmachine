2887902f727ab924d0f0691d5cb3b3420256aed3
kamasamikon <kamasamikon@gmail.com>
2015-02-11 18:03:19 +0800

    dddddddddddd

--- mk.tools/inst.busybox.sh
+++ mk.tools/inst.busybox.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
-TARGETDIR=shit
+BLD_TYPE=`cat .bld_type`
+CFG_TYPE=`cat .cfg_type`
+
+BIN_DIR=../${BLD_TYPE}_${CFG_TYPE}/target/bin
+
+if [ ! -e $BIN_DIR ]; then
+    echo "Set CONFIG_TYPE to .cfg_type"
+    echo "Set BULID_TYPE to .bld_type"
+    exit
+fi
 
 for applet in `busybox --list`; do
-    ln -s /bin/busybox $TARGETDIR/$applet
+    ln -s /bin/busybox $BIN_DIR/$applet
 done
 
