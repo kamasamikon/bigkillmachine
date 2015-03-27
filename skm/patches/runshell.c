a785f95e6fd13bf9bb1b6cacb9083fe7b13efc00
kamasamikon <kamasamikon@gmail.com>
2015-03-27 18:38:41 +0800

    runproc: use setenv to replace execvpe

--- skm/patches/runshell.c
+++ skm/patches/runshell.c
@@ -4,13 +4,18 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-/* runproc(1, argv[0], ..., argv[Last], NULL, envp[0], ..., envp[Last], NULL */
+/*
+ * runproc(1, argv[0], ..., argv[Last], NULL, envk[0], envv[0], ..., NULL
+ * runproc(1, "/bin/sh", NULL, "PS1", "SKM>  ", NULL);
+ * runproc(1, "/bin/cat", "/BUILDINFO", NULL, NULL);
+ */
 static void runproc(int wait, const char *arg, ...)
 {
 	pid_t pid;
 
-	int argc = 0, envc = 0;
-	const char *argv[1024], *envv[1024], *tmp;
+	/* env's key value */
+	int i, argc = 0, ekvc = 0;
+	const char *argv[1024], *ekvv[1024], *tmp;
 
 	va_list ap;
 
@@ -22,14 +27,14 @@ static void runproc(int wait, const char *arg, ...)
 		argv[argc++] = tmp;
 		tmp = va_arg(ap, char *);
 	}
-    argv[argc] = NULL;
+	argv[argc] = NULL;
 
 	tmp = va_arg(ap, char *);
 	while (tmp) {
-		envv[envc++] = tmp;
+		ekvv[ekvc++] = tmp;
 		tmp = va_arg(ap, char *);
 	}
-	envv[envc] = NULL;
+	ekvv[ekvc] = NULL;
 
 	va_end(ap);
 
@@ -39,7 +44,10 @@ static void runproc(int wait, const char *arg, ...)
 	} else if (pid == 0) {
 		printf("Will run '%s'\n", argv[0]);
 
-		execvpe(argv[0], argv, envv);
+		for (i = 0; i < ekvc - 1; i += 2)
+			setenv(ekvv[i], ekvv[i + 1], 1);
+
+		execvp(argv[0], (char * const *)argv);
 		_exit(0);
 	}
 
@@ -47,14 +55,5 @@ static void runproc(int wait, const char *arg, ...)
 		if (waitpid(pid, NULL, 0) != pid) {
 			printf("waitpid error.\n");
 		}
-}
-
-int main(int argc, char *argv[])
-{
-	// runproc(1, "/bin/sh", NULL, "PS1=SKM> ", NULL);
-	// runproc(1, "/bin/sh", NULL, NULL);
-    runproc(1, "/bin/cat", "/BUILDINFO", NULL, NULL);
-    runproc(1, "/bin/sh", NULL, "PS1=SKM>  ", NULL);
-	runproc(1, "/bin/sh", NULL);
 }
 
