fb43655a4df6d5508b73b3b8524874560bece4fe
kamasamikon <kamasamikon@qq.com>
2016-07-08 11:12:10 +0800

    inotdo reborn

--- SRCS/inotdo/inotdo.c
+++ SRCS/inotdo/inotdo.c
@@ -11,10 +11,14 @@
 
 typedef struct _watchinf_s watchinf_s;
 struct _watchinf_s {
+	unsigned int mask;
 	int wd;
 	int msc_idx;
 	int cmd_idx;
 	char *file;
+
+	/* Event occured for this */
+	int dirty;
 };
 
 static int __argc;
@@ -24,8 +28,9 @@ static watchinf_s *__wchinf = NULL;
 static int __wchinf_cnt = 0;
 
 static int __debug_mode = 0;
+static int __silent_call = 0;
 
-static void set_subenv(struct inotify_event *event);
+static void set_subenv(struct inotify_event *ev);
 
 /* Mask array and Command arrary */
 int __msc_cnt = 0;
@@ -76,23 +81,25 @@ static unsigned int uptime(void)
 	return retval;
 }
 
-static pid_t run_command(char *const argv, struct inotify_event *event)
+static pid_t run_command(char *const argv, struct inotify_event *ev)
 {
 	pid_t pid;
 	int ret;
 
 	pid = fork();
 	if (0 == pid) {
-		set_subenv(event);
+		set_subenv(ev);
 
 		/* child process, execute the command */
 #if 0
 		execvp(argv[0], argv);
 		_exit(EXIT_FAILURE);
 #else
-		printf("================================================================\n");
+		if (!__silent_call)
+			printf(">>> WBG: '%s' start.\n", argv);
 		ret = system(argv);
-		printf("----------------------------------------------------------------\n");
+		if (!__silent_call)
+			printf("<<< WBG: '%s' end.\n", argv);
 		exit(ret);
 #endif
 	}
@@ -144,141 +151,115 @@ static unsigned int get_mask(const char *maskstr)
 	return mask;
 }
 
-static void dump_event(struct inotify_event *event)
+static void event_parse(struct inotify_event *ev,
+		char file[1024], char name[1024], char mask[1024])
 {
-	char mask[1024];
+	char buf[1024];
 	int bytes = 0;
 
-	if (event->mask & IN_ACCESS)
-		bytes += sprintf(&mask[bytes], " %s |", "ACCESS");
-	if (event->mask & IN_MODIFY)
-		bytes += sprintf(&mask[bytes], " %s |", "MODIFY");
-	if (event->mask & IN_ATTRIB)
-		bytes += sprintf(&mask[bytes], " %s |", "ATTRIB");
-	if (event->mask & IN_CLOSE_WRITE)
-		bytes += sprintf(&mask[bytes], " %s |", "CLOSE_WRITE");
-	if (event->mask & IN_CLOSE_NOWRITE)
-		bytes += sprintf(&mask[bytes], " %s |", "CLOSE_NOWRITE");
-	if (event->mask & IN_OPEN)
-		bytes += sprintf(&mask[bytes], " %s |", "OPEN");
-	if (event->mask & IN_MOVED_FROM)
-		bytes += sprintf(&mask[bytes], " %s |", "MOVED_FROM");
-	if (event->mask & IN_MOVED_TO)
-		bytes += sprintf(&mask[bytes], " %s |", "MOVED_TO");
-	if (event->mask & IN_CREATE)
-		bytes += sprintf(&mask[bytes], " %s |", "CREATE");
-	if (event->mask & IN_DELETE)
-		bytes += sprintf(&mask[bytes], " %s |", "DELETE");
-	if (event->mask & IN_DELETE_SELF)
-		bytes += sprintf(&mask[bytes], " %s |", "DELETE_SELF");
-	if (event->mask & IN_MOVE_SELF)
-		bytes += sprintf(&mask[bytes], " %s |", "MOVE_SELF");
+	file[0] = '\0';
+	name[0] = '\0';
+	mask[0] = '\0';
 
-	if (event->mask & IN_UNMOUNT)
-		bytes += sprintf(&mask[bytes], " %s |", "UNMOUNT");
-	if (event->mask & IN_Q_OVERFLOW)
-		bytes += sprintf(&mask[bytes], " %s |", "Q_OVERFLOW");
-	if (event->mask & IN_IGNORED)
-		bytes += sprintf(&mask[bytes], " %s |", "IGNORED");
+	if (ev->mask & IN_ACCESS)
+		bytes += sprintf(&buf[bytes], " %s |", "ACCESS");
+	if (ev->mask & IN_MODIFY)
+		bytes += sprintf(&buf[bytes], " %s |", "MODIFY");
+	if (ev->mask & IN_ATTRIB)
+		bytes += sprintf(&buf[bytes], " %s |", "ATTRIB");
+	if (ev->mask & IN_CLOSE_WRITE)
+		bytes += sprintf(&buf[bytes], " %s |", "CLOSE_WRITE");
+	if (ev->mask & IN_CLOSE_NOWRITE)
+		bytes += sprintf(&buf[bytes], " %s |", "CLOSE_NOWRITE");
+	if (ev->mask & IN_OPEN)
+		bytes += sprintf(&buf[bytes], " %s |", "OPEN");
+	if (ev->mask & IN_MOVED_FROM)
+		bytes += sprintf(&buf[bytes], " %s |", "MOVED_FROM");
+	if (ev->mask & IN_MOVED_TO)
+		bytes += sprintf(&buf[bytes], " %s |", "MOVED_TO");
+	if (ev->mask & IN_CREATE)
+		bytes += sprintf(&buf[bytes], " %s |", "CREATE");
+	if (ev->mask & IN_DELETE)
+		bytes += sprintf(&buf[bytes], " %s |", "DELETE");
+	if (ev->mask & IN_DELETE_SELF)
+		bytes += sprintf(&buf[bytes], " %s |", "DELETE_SELF");
+	if (ev->mask & IN_MOVE_SELF)
+		bytes += sprintf(&buf[bytes], " %s |", "MOVE_SELF");
 
-	if (event->mask & IN_ONLYDIR)
-		bytes += sprintf(&mask[bytes], " %s |", "ONLYDIR");
-	if (event->mask & IN_DONT_FOLLOW)
-		bytes += sprintf(&mask[bytes], " %s |", "DONT_FOLLOW");
+	if (ev->mask & IN_UNMOUNT)
+		bytes += sprintf(&buf[bytes], " %s |", "UNMOUNT");
+	if (ev->mask & IN_Q_OVERFLOW)
+		bytes += sprintf(&buf[bytes], " %s |", "Q_OVERFLOW");
+	if (ev->mask & IN_IGNORED)
+		bytes += sprintf(&buf[bytes], " %s |", "IGNORED");
+
+	if (ev->mask & IN_ONLYDIR)
+		bytes += sprintf(&buf[bytes], " %s |", "ONLYDIR");
+	if (ev->mask & IN_DONT_FOLLOW)
+		bytes += sprintf(&buf[bytes], " %s |", "DONT_FOLLOW");
 #if 0
-	if (event->mask & IN_EXCL_UNLINK)
-		bytes += sprintf(&mask[bytes], "%s |", "EXCL_UNLINK");
+	if (ev->mask & IN_EXCL_UNLINK)
+		bytes += sprintf(&buf[bytes], "%s |", "EXCL_UNLINK");
 #endif
 
-	if (event->mask & IN_MASK_ADD)
-		bytes += sprintf(&mask[bytes], " %s |", "MASK_ADD");
-	if (event->mask & IN_ISDIR)
-		bytes += sprintf(&mask[bytes], " %s |", "ISDIR");
-	if (event->mask & IN_ONESHOT)
-		bytes += sprintf(&mask[bytes], " %s |", "ONESHOT");
+	if (ev->mask & IN_MASK_ADD)
+		bytes += sprintf(&buf[bytes], " %s |", "MASK_ADD");
+	if (ev->mask & IN_ISDIR)
+		bytes += sprintf(&buf[bytes], " %s |", "ISDIR");
+	if (ev->mask & IN_ONESHOT)
+		bytes += sprintf(&buf[bytes], " %s |", "ONESHOT");
 
-	watchinf_s *wi = watchinf_find(event->wd);
+	watchinf_s *wi = watchinf_find(ev->wd);
 	if (wi)
-		logmsg("> file : %s\n", wi->file);
+		strcpy(file, wi->file);
 
+	if (ev->len)
+		strcpy(name, ev->name);
 
 	if (bytes) {
-		mask[bytes - 2] = '\0';
-		logmsg("> mask :%s\n\n", mask);
-	} else {
-		logmsg("> mask :%s\n\n", " (null)");
+		buf[bytes - 2] = '\0';
+		strcpy(mask, &buf[1]);
 	}
 }
 
-static void set_subenv(struct inotify_event *event)
+static void dump_event(struct inotify_event *ev)
 {
-	char mask[1024];
-	int bytes = 0;
+	char file[1024], name[1024], mask[1024];
 
-	char envname[256], envmask[256];
+	event_parse(ev, file, name, mask);
 
-	sprintf(envname, "WBG_NAME");
-	sprintf(envmask, "WBG_MASK");
+	if (file[0])
+		logmsg("> file: %s\n", file);
 
-	if (event->mask & IN_ACCESS)
-		bytes += sprintf(&mask[bytes], " %s |", "ACCESS");
-	if (event->mask & IN_MODIFY)
-		bytes += sprintf(&mask[bytes], " %s |", "MODIFY");
-	if (event->mask & IN_ATTRIB)
-		bytes += sprintf(&mask[bytes], " %s |", "ATTRIB");
-	if (event->mask & IN_CLOSE_WRITE)
-		bytes += sprintf(&mask[bytes], " %s |", "CLOSE_WRITE");
-	if (event->mask & IN_CLOSE_NOWRITE)
-		bytes += sprintf(&mask[bytes], " %s |", "CLOSE_NOWRITE");
-	if (event->mask & IN_OPEN)
-		bytes += sprintf(&mask[bytes], " %s |", "OPEN");
-	if (event->mask & IN_MOVED_FROM)
-		bytes += sprintf(&mask[bytes], " %s |", "MOVED_FROM");
-	if (event->mask & IN_MOVED_TO)
-		bytes += sprintf(&mask[bytes], " %s |", "MOVED_TO");
-	if (event->mask & IN_CREATE)
-		bytes += sprintf(&mask[bytes], " %s |", "CREATE");
-	if (event->mask & IN_DELETE)
-		bytes += sprintf(&mask[bytes], " %s |", "DELETE");
-	if (event->mask & IN_DELETE_SELF)
-		bytes += sprintf(&mask[bytes], " %s |", "DELETE_SELF");
-	if (event->mask & IN_MOVE_SELF)
-		bytes += sprintf(&mask[bytes], " %s |", "MOVE_SELF");
+	if (name[0])
+		logmsg("> name: %s\n", name);
 
-	if (event->mask & IN_UNMOUNT)
-		bytes += sprintf(&mask[bytes], " %s |", "UNMOUNT");
-	if (event->mask & IN_Q_OVERFLOW)
-		bytes += sprintf(&mask[bytes], " %s |", "Q_OVERFLOW");
-	if (event->mask & IN_IGNORED)
-		bytes += sprintf(&mask[bytes], " %s |", "IGNORED");
-
-	if (event->mask & IN_ONLYDIR)
-		bytes += sprintf(&mask[bytes], " %s |", "ONLYDIR");
-	if (event->mask & IN_DONT_FOLLOW)
-		bytes += sprintf(&mask[bytes], " %s |", "DONT_FOLLOW");
-#if 0
-	if (event->mask & IN_EXCL_UNLINK)
-		bytes += sprintf(&mask[bytes], "%s |", "EXCL_UNLINK");
-#endif
-
-	if (event->mask & IN_MASK_ADD)
-		bytes += sprintf(&mask[bytes], " %s |", "MASK_ADD");
-	if (event->mask & IN_ISDIR)
-		bytes += sprintf(&mask[bytes], " %s |", "ISDIR");
-	if (event->mask & IN_ONESHOT)
-		bytes += sprintf(&mask[bytes], " %s |", "ONESHOT");
-
-	watchinf_s *wi = watchinf_find(event->wd);
-	if (wi)
-		setenv(envname, wi->file, 1);
-
-	if (bytes) {
-		mask[bytes - 2] = '\0';
-		setenv(envmask, &mask[1], 1);
-	} else {
-		setenv(envmask, "(null)", 1);
-	}
+	logmsg("> mask: %s\n", mask[0] ? mask : " (null)");
 }
+
+
+static void set_subenv(struct inotify_event *ev)
+{
+	char file[1024], name[1024], mask[1024];
+
+	event_parse(ev, file, name, mask);
+
+	if (file[0])
+		setenv("WBG_FILE", file, 1);
+	else
+		unsetenv("WBG_FILE");
+
+	if (name[0])
+		setenv("WBG_NAME", name, 1);
+	else
+		unsetenv("WBG_NAME");
+
+	if (mask[0])
+		setenv("WBG_MASK", mask, 1);
+	else
+		unsetenv("WBG_MASK");
+}
+
 
 static int rule_split(const char *rule, char mask[256], char comm[256])
 {
@@ -328,11 +309,13 @@ int main(int argc, char *argv[])
 
 	int poll_timeout = 1000;
 
+	int reload_mode = 0;
+
 	__argc = argc;
 	__argv = argv;
 
 	if (argc < 4) {
-		printf("usage: inotdo file mask command ...\n");
+		printf("usage: inotdo [-reload] [-token token-string] [-s] [-v | -nov] -m<mask> mask [-c<cmd> command] -r,<mask>,<cmd> files ...");
 		printf("mask:\n");
 		printf("        access : File was accessed.\n");
 		printf("        modify : File was modified.\n");
@@ -350,10 +333,20 @@ int main(int argc, char *argv[])
 		printf("     move_self : Self was moved.\n");
 		printf("           all : Ored all above.\n");
 		printf("\n");
+		printf("        -token : A string.\n");
+		printf("       -reload : Re-watch the file after hit with same mask.\n");
+		printf("            -s : Slient mode: No log message when call the commands.\n");
+		printf("     -v / -nov : Verbose mode on and off.\n");
+		printf("\n");
 		printf("env[WBG_DEBUG] : Show verbose.\n");
 		printf("\n");
 		printf("e.g.: inotdo -m1 access,move -ca 'echo \"xxx\"; ls' -r,1,a /tmp/abc ~/haha.c ... \n");
 		return -1;
+	}
+
+	if (getenv("WBG_DEBUG")) {
+		printf("WBG: WBG_DEBUG set.\n");
+		__debug_mode = 1;
 	}
 
 	__msc_name = (char**)calloc(argc, sizeof(char*));
@@ -363,6 +356,31 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++) {
 		char *arg = argv[i];
+
+		if (!strcmp(arg, "-token")) {
+			argv[i] = NULL;
+			argv[i + 1] = NULL;
+
+			i++;
+		}
+
+		if (!strcmp(arg, "-reload")) {
+			argv[i] = NULL;
+			reload_mode = 1;
+		}
+
+		if (!strcmp(arg, "-s")) {
+			argv[i] = NULL;
+			__silent_call = 1;
+		}
+		if (!strcmp(arg, "-v")) {
+			argv[i] = NULL;
+			__debug_mode = 1;
+		}
+		if (!strcmp(arg, "-nov")) {
+			argv[i] = NULL;
+			__debug_mode = 0;
+		}
 
 		if (arg[0] == '-' && arg[1] == 'm') {
 			__msc_name[__msc_cnt] = &arg[2];
@@ -425,7 +443,7 @@ int main(int argc, char *argv[])
 				break;
 		}
 		if (j == __msc_cnt) {
-			printf("Bad mask '%s' for [%s].\n", cur_mask, arg);
+			printf("Bad mask '%s' for [%s].\n", cur_mask, file);
 			continue;
 		}
 
@@ -434,8 +452,8 @@ int main(int argc, char *argv[])
 				break;
 		}
 		if (k == __cmd_idx) {
-			printf("Bad command '%s' for [%s].\n", cur_comm, arg);
-			continue;
+			printf("No command '%s' for [%s].\n", cur_comm, file);
+			k = -1;
 		}
 
 		mask = get_mask(__msc_content[j]);
@@ -447,59 +465,85 @@ int main(int argc, char *argv[])
 		}
 
 		wi = &__wchinf[__wchinf_cnt++];
+		wi->mask = mask;
 		wi->wd = wd;
 		wi->msc_idx = j;
 		wi->cmd_idx = k;
 		wi->file = file;
+		wi->dirty = 0;
 	}
 
 
 	size_t evbuflen = argc * 2 * sizeof(struct inotify_event);
 	unsigned char *evbuf = (unsigned char*)malloc(evbuflen);
 
-	if (getenv("WBG_DEBUG")) {
-		printf("WBG: WBG_DEBUG set.\n");
-		__debug_mode = 1;
-	}
-
 	while (1) {
 		struct pollfd pfd = { fd, POLLIN, 0 };
 		int ret = poll(&pfd, 1, poll_timeout);
 		if (ret < 0) {
+			/* Error */
 			fprintf(stderr, "poll failed: %s\n", strerror(errno));
 			continue;
 		}
+		if (ret == 0) {
+			/* Timeout */
+			continue;
+		}
 
-		if (ret > 0) {
-			bytes = read(fd, (void*)evbuf, evbuflen);
-			if (-1 == bytes) {
-				fprintf(stderr, "read NG: %s\n", strerror(errno));
-				goto clean_quit;
-			}
+		bytes = read(fd, (void*)evbuf, evbuflen);
+		if (-1 == bytes) {
+			fprintf(stderr, "read NG: %s\n", strerror(errno));
+			goto clean_quit;
+		}
 
-			int i, ofs = 0;
-			struct inotify_event *ev;
+		int i, ofs = 0;
+		struct inotify_event *ev;
 
-			while (ofs < bytes) {
-				ev = (struct inotify_event*)(void*)(&evbuf[ofs]);
-				if (__debug_mode)
+		while (ofs < bytes) {
+			ev = (struct inotify_event*)(void*)(&evbuf[ofs]);
+
+			for (i = 0; i < __wchinf_cnt; i++) {
+				wi = &__wchinf[i];
+				if (wi->wd != ev->wd)
+					continue;
+
+				wi->dirty = 1;
+
+				/* IN_IGNORED etc should be skipped */
+				if (!(wi->mask & ev->mask))
+					continue;
+
+				if (__debug_mode) {
+					logmsg("\nWD:%d, CMD: [%s]\n", wi->wd, command);
 					dump_event(ev);
-
-				for (i = 0; i < __wchinf_cnt; i++) {
-					wi = &__wchinf[i];
-					if (wi->wd != ev->wd)
-						continue;
-
-					command = __cmd_content[wi->cmd_idx];
-					logmsg("WD:%d, CMD: [%s]\n", wi->wd, command);
-					if (command && command[0])
-						run_command(command, ev);
-					break;
 				}
 
-				ofs += sizeof(struct inotify_event) + ev->len;
+				if (wi->cmd_idx >= 0)
+					command = __cmd_content[wi->cmd_idx];
+				else
+					command = NULL;
+
+				if (command && command[0])
+					run_command(command, ev);
 			}
 
+			ofs += sizeof(struct inotify_event) + ev->len;
+		}
+
+		/*
+		 * XXX: Delete and readd, it will
+		 * caused many INGORED event
+		 */
+		if (reload_mode) {
+			for (i = 0; i < argc - 3; i++) {
+				wi = &__wchinf[i];
+				if (!wi->dirty)
+					continue;
+
+				wi->dirty = 0;
+				inotify_rm_watch(fd, wi->wd);
+				wi->wd = inotify_add_watch(fd, wi->file, wi->mask);
+			}
 		}
 	}
 
@@ -507,8 +551,6 @@ clean_quit:
 	if (__wchinf) {
 		for (i = 0; i < argc - 3; i++) {
 			wi = &__wchinf[i];
-			if (wi->wd < 0)
-				break;
 			inotify_rm_watch(fd, wd);
 		}
 		free(__wchinf);
